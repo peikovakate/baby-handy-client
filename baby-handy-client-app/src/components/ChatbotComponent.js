707dd0137610c5aa0d2d4bc9f019dfbd33115ed2
peikovakate <peikovakate@gmail.com>
2018-10-12 12:27:52 +0300

    added chatbot example

--- baby-handy-client-app/src/components/ChatbotComponent.js
+++ baby-handy-client-app/src/components/ChatbotComponent.js
@@ -1,7 +1,54 @@
 import React from "react"
+import ChatBot from 'react-simple-chatbot';
 
 const ChatBotComponent = props => (
-    <div>This is a chat bot component</div>
+    <div>
+        <p>This is a chat bot component</p>
+        <ChatBot
+            steps={[
+                {
+                id: 'hello-world',
+                message: 'Hello World!',
+                end: true,
+                },
+            ]}
+            />
+    </div>
 );
 
 export default ChatBotComponent
+
+// import React from 'react';
+// import { ThemeProvider } from 'styled-components';
+// // import ChatBot from '../../lib/index';
+// import ChatBot from 'react-simple-chatbot';
+
+
+// const otherFontTheme = {
+//   background: '#f5f8fb',
+//   fontFamily: 'Helvetica Neue',
+//   headerBgColor: '#6e48aa',
+//   headerFontColor: '#fff',
+//   headerFontSize: '16px',
+//   botBubbleColor: '#6E48AA',
+//   botFontColor: '#fff',
+//   userBubbleColor: '#fff',
+//   userFontColor: '#4a4a4a',
+// };
+
+// const steps = [
+//   {
+//     id: '1',
+//     message: 'Hello World',
+//     end: true,
+//   },
+// ];
+
+
+// const ChatBotComponent = () => (
+//   <ThemeProvider theme={otherFontTheme}>
+//     <ChatBot steps={steps} />
+//   </ThemeProvider>
+// );
+
+// export default ChatBotComponent;
