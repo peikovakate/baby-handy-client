c91c0df17e2f7d43120df186fd1f8284d42ae45f
peikovakate <peikovakate@gmail.com>
2018-10-28 01:45:38 +0300

    changed signin and signup components: added fields; added material design style

--- baby-handy-client-app/src/components/SignIn.js
+++ baby-handy-client-app/src/components/SignIn.js
@@ -1,9 +1,43 @@
-import React from "react"
+import React, {Component} from "react"
 
-const SignIn = props => (
-    <div>
-        <p>This is a SignIn component</p>
-    </div>
-);
+class SignIn extends Component{
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state )
+
+    }
+
+    render(){
+        return(
+             <div className='container'>
+                 <form onSubmit = {this.handleSubmit} >
+                    <h5>Sign In</h5>
+                    <div className ="input-field">
+                        <label  htmlFor='email'>Email</label>
+                        <input type="email" id="email" onChange={this.handleChange}/>
+                    </div>
+                    <div className ="input-field">
+                        <label  htmlFor='password'>Password</label>
+                        <input type="password" id="password" onChange={this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <button className="btn waves-effect waves-light" >Login</button>
+                    </div>
+                 </form>
+             </div>
+        )
+    }
+}
 
 export default SignIn
