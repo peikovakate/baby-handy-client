0e4de74c9e6b6424d1a630eba47f1a2156ca93cf
peikovakate <peikovakate@gmail.com>
2018-10-31 12:34:59 +0200

    added root reducer and redux store

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const About = props => (
+    <div>
+        <p>This is a About component</p>
+    </div>
+);
+
+export default About
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { NavLink } from 'react-router-dom'
+
+// Header, some panel will be here
+const Header = props => (
+  <header>
+    <nav>
+      <ul className='right'>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/signin'>Sign In</NavLink></li>
+        <li><NavLink to='/signup'>Sign Up</NavLink></li>
+        <li><NavLink to='/addchild'>Add child</NavLink></li>
+        <li><NavLink to='/' className='btn btn-floating grey'>NN</NavLink></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header
