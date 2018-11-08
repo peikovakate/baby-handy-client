117ccde7655404ff86fdc7c9f0e65509fb52ae37
peikovakate <peikovakate@gmail.com>
2018-11-08 19:48:14 +0200

    header changed, added logout button

--- src/_components/Header.js
+++ src/_components/Header.js
@@ -1,21 +1,41 @@
 import React from "react"
 import { NavLink } from 'react-router-dom'
 
-// Header, some panel will be here
-export const Header = props => (
-  <header>
-    <nav>
-      <ul className='right'>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink to='/signin'>Sign In</NavLink></li>
-        <li><NavLink to='/signup'>Sign Up</NavLink></li>
-        <li><NavLink to='/addchild'>Add child</NavLink></li>
-        <li><NavLink to='/childlist'>Child List</NavLink></li>
-        <li><NavLink to='/' className='btn btn-floating grey'>NN</NavLink></li>
-      </ul>
-    </nav>
-  </header>
-);
+// component that decide what buttons to show depending on if user logged in
+class HeaderLinks extends React.Component{
+  render(){
+    if (localStorage.getItem('user')) {
+      return(
+         <ul className='right'> 
+          <li><NavLink to='/addchild'>Add child</NavLink></li> 
+          <li><NavLink to='/childlist'>Child List</NavLink></li> 
+          <li><NavLink to='/signin'>Log Out</NavLink></li>
+          {/* <li><NavLink to='/' className='btn btn-floating grey'>NN</NavLink></li>  */}
+        </ul>);
+    }else {
+      return(
+        <ul className='right'> 
+          <li><NavLink to='/signin'>Sign In</NavLink></li>
+          <li><NavLink to='/signup'>Sign Up</NavLink></li>
+        </ul>)
+    }
+  }
+}
 
-// export default Header
+// Header, some panel will be here
+export class Header extends React.Component{
+  render(){
+    return(
+    <header>
+      <nav>
+        <ul className='right'>    
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <HeaderLinks/>
+        </ul>
+      </nav>
+    </header>
+    )}  
+}
+
+export default Header
